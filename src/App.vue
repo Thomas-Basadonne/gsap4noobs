ab7ceea8025c4fb40b70796b9dc6d3143faf7d61
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2023-12-03 23:06:22 +0100

    init vue app + tailwind + gsap

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { onMounted } from "vue";
+import { gsap } from "gsap";
+
+onMounted(() => {
+  gsap.to(".p", {
+    x: 200,
+  });
+});
+</script>
+
+<template>
+  <h1 class="p text-3xl font-bold underline">Hello world!</h1>
+</template>
+
+<style scoped></style>
