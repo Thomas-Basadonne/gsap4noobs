355adccb7d9de1e2bb308925e5b12d9aa0e65354
Thomas-Basadonne <thommybasa@gmail.com>
2023-12-04 15:06:02 +0100

    init style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // backgroundColor: {
+      //   app: "#0E100F",
+      // },
+      // textColor: {
+      //   app: "#FFFCE2",
+      // },
+      colors: {
+        sfondo: "#0E100F",
+        testo: "#FFFCE2",
+        "doc-bg": "#0D100F",
+        verde: "#1AE348",
+        sky: "#17BAE2",
+        pink: "#FEC5FC",
+        purple: "#9E95FF",
+        orange: "#F9840A",
+      },
+    },
   },
-  plugins: [],
 };
