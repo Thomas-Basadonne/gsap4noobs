7cbb75126068847e69a112ac8878fedf47598ae8
Thomas-Basadonne <91781656+Thomas-Basadonne@users.noreply.github.com>
2023-12-04 16:02:41 +0100

    header app init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
-<script setup>
+<script>
 import { onMounted } from "vue";
 import { gsap } from "gsap";
+import HeaderApp from "./components/_partials/HeaderApp.vue";
+import FooterApp from "./components/_partials/FooterApp.vue";
 
-onMounted(() => {
-  gsap.to(".p", {
-    x: 200,
-  });
-});
+export default {
+  data() {
+    return {
+      title: "Gsap4Noobs",
+    };
+  },
+  components: {
+    HeaderApp,
+    FooterApp,
+  },
+};
 </script>
 
 <template>
+  <HeaderApp />
   <h1 class="p text-3xl font-bold underline">Hello world!</h1>
 </template>
 
--- src/components/_partials/HeaderApp.vue
+++ src/components/_partials/HeaderApp.vue
@@ -1,21 +1,52 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
+// import  from "./components/MyComponent.vue";
+import { gsap } from "gsap";
 
 export default {
   data() {
     return {
-      title: "Hello world"
-    }
-  }
+      title: "Hello world",
+    };
+  },
+  mounted() {
+    gsap.set(["a", "li"], {
+      autoAlpha: 0,
+      y: -200,
+    });
 
-  // components: {
-  //   MyComponent,
-  // },
+    gsap.to("a", {
+      autoAlpha: 1,
+      y: 0,
+      duration: 2,
+      ease: "bounce",
+    });
+
+    gsap.to("li", {
+      autoAlpha: 1,
+      y: 0,
+      duration: 2,
+      ease: "power2.out",
+      stagger: 0.2,
+    });
+  },
 };
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+  <header
+    class="mx-6 my-4 pb-4 flex flex-col items-center border-b border-testo sm:flex-row sm:justify-between sm:items-center"
+  >
+    <a href="/" class="text-2xl font-black">
+      GSAP<span class="text-verde mx-1">4</span>NOOBS
+    </a>
+
+    <ul class="nav hidden sm:flex sm:flex-row sm:gap-4">
+      <li><a href="http://">Showcase</a></li>
+      <li><a href="http://">Install</a></li>
+      <li><a href="http://">Docs</a></li>
+      <li><a href="http://">Contacts</a></li>
+    </ul>
+  </header>
 </template>
 
 <style lang="scss" scoped></style>
